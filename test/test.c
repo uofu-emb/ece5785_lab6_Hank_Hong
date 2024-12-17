7c35c3ab8a7628b89c0eccc2a688cec08fe2195b
Hank <henryhanksthompson@gmail.com>
2024-12-16 20:35:43 -0700

    finished running tests

--- test/test.c
+++ test/test.c
@@ -1,35 +1,164 @@
+/**
+ * Copyright (c) 2022 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
 #include <stdio.h>
 #include <pico/stdlib.h>
-#include <stdint.h>
-#include <unity.h>
+#include <FreeRTOS.h>
+#include <task.h>
+#include <semphr.h>
+
 #include "unity_config.h"
+#include <unity.h>
 
-void setUp(void) {}
+SemaphoreHandle_t semaphore;
 
-void tearDown(void) {}
+TaskHandle_t firstThread;
+TaskHandle_t secondThread;
 
-void test_variable_assignment()
-{
-    int x = 1;
-    TEST_ASSERT_TRUE_MESSAGE(x == 1,"Variable assignment failed.");
+TaskFunction_t primary;
+TaskFunction_t secondary;
+
+configRUN_TIME_COUNTER_TYPE first_stats, second_stats, elapsed_stats;
+TickType_t elapsed_ticks;
+
+void setUp(void) {
 }
 
-void test_multiplication(void)
-{
-    int x = 30;
-    int y = 6;
-    int z = x / y;
-    TEST_ASSERT_TRUE_MESSAGE(z == 5, "Multiplication of two integers returned incorrect value.");
+void tearDown(void) {
 }
 
-int main (void)
+void priorityInversionBinary() {
+    xSemaphoreTake(semaphore, portMAX_DELAY);
+
+    semaphore = xSemaphoreCreateBinary();
+
+    xTaskCreate(primary, "First", configMINIMAL_STACK_SIZE, "FirstThread", tskIDLE_PRIORITY+(1), &firstThread);
+    vTaskDelay(100);
+
+    xTaskCreate(secondary, "Second", configMINIMAL_STACK_SIZE, "SecondThread", tskIDLE_PRIORITY+(2), &secondThread);
+
+    busy_wait_us(1000);
+
+    xSemaphoreGive(semaphore);
+
+    vSemaphoreDelete(semaphore);
+}
+
+void priorityInversionMutex() {
+    xSemaphoreTake(semaphore, portMAX_DELAY);
+
+    semaphore = xSemaphoreCreateMutex();
+
+    xTaskCreate(primary, "First", configMINIMAL_STACK_SIZE, "FirstThread", tskIDLE_PRIORITY+(1), &firstThread);
+    vTaskDelay(100);
+
+    xTaskCreate(secondary, "Second", configMINIMAL_STACK_SIZE, "SecondThread", tskIDLE_PRIORITY+(2), &secondThread);
+
+    busy_wait_us(1000);
+
+    xSemaphoreGive(semaphore);
+
+    vSemaphoreDelete(semaphore);
+}
+
+void busy_busy(void *args) {
+    char *name = (char *)args;
+    printf("busy_busy %s\n", name);
+    for(int i = 0; ; i++) {
+    }
+}
+
+void busy_yield(void *args) {
+    char *name = (char *)args;
+    printf("busy_yield %s\n", name);
+    for(int i = 0; ; i++) {
+        taskYIELD();
+    }
+}
+
+void testSamePriorityBothBusyBusy() {
+     run_analyzer(busy_busy, tskIDLE_PRIORITY+(3), 0, &first_stats,
+                 busy_busy, tskIDLE_PRIORITY+(3), 0, &second_stats,
+                 &elapsed_stats, &elapsed_ticks);
+    TEST_ASSERT(2000000 < first_stats);
+    TEST_ASSERT(2000000 < second_stats);
+}
+
+void testSamePriorityBothBusyYield() {
+    run_analyzer(busy_yield, tskIDLE_PRIORITY+(3), 0, &first_stats,
+                 busy_yield, tskIDLE_PRIORITY+(3), 0, &second_stats,
+                 &elapsed_stats, &elapsed_ticks);
+    TEST_ASSERT(2000000 < first_stats);
+    TEST_ASSERT(2000000 < second_stats);
+
+}
+
+void testSamePriorityBusyBusy_BusyYield() {
+     run_analyzer(busy_busy, tskIDLE_PRIORITY+(3), 0, &first_stats,
+                 busy_yield, tskIDLE_PRIORITY+(3), 0, &second_stats,
+                 &elapsed_stats, &elapsed_ticks);
+    TEST_ASSERT(2000000 < first_stats);
+    TEST_ASSERT(2000000 < second_stats);
+
+}
+
+void testDifferentPriorityBothBusyBusy() {
+     run_analyzer(busy_busy, tskIDLE_PRIORITY+(3), 1, &first_stats,
+                 busy_busy, tskIDLE_PRIORITY+(3), 0, &second_stats,
+                 &elapsed_stats, &elapsed_ticks);
+    TEST_ASSERT(2000000 < first_stats);
+    TEST_ASSERT(2000000 < second_stats);
+
+}
+
+void testDifferentPriorityBothBusyYield() {
+     run_analyzer(busy_yield, tskIDLE_PRIORITY+(3), 1, &first_stats,
+                 busy_yield, tskIDLE_PRIORITY+(3), 0, &second_stats,
+                 &elapsed_stats, &elapsed_ticks);
+    TEST_ASSERT(2000000 < first_stats);
+    TEST_ASSERT(2000000 < second_stats);
+
+}
+
+void measure_runtime(void (func0)(void), UBaseType_t priority0,
+                     void (func1)(void), UBaseType_t priority1,
+                     configRUN_TIME_COUNTER_TYPE runtime0, configRUN_TIME_COUNTER_TYPEruntime1)
+{
+    TaskHandle_t handle0, handle1;
+    xTaskCreate(simple_task, "Thread0", configMINIMAL_STACK_SIZE, func0, priority0, &handle0);
+    xTaskCreate(delayed_task, "Thread1", configMINIMAL_STACK_SIZE, func1, priority1, &handle1);
+
+    vTaskDelay(2000);
+
+    TaskStatus_t status;
+    vTaskGetInfo(handle0, &status, pdFALSE, eInvalid);
+    *runtime0 = status.ulRunTimeCounter;
+    vTaskGetInfo(handle1, &status, pdFALSE, eInvalid);
+    *runtime1 = status.ulRunTimeCounter;
+
+    vTaskDelete(handle0);
+    vTaskDelete(handle1);
+}
+
+void tests() {
+    UNITY_BEGIN();
+    RUN_TEST(testSamePriorityBothBusyBusy);
+    RUN_TEST(testSamePriorityBothBusyYield);
+    RUN_TEST(testSamePriorityBusyBusy_BusyYield);
+    RUN_TEST(testDifferentPriorityBothBusyBusy);
+    RUN_TEST(testDifferentPriorityBothBusyYield);
+    UNITY_END();
+    vTaskDelay(1000);
+}
+
+int main( void )
 {
     stdio_init_all();
-    sleep_ms(5000); // Give time for TTY to attach.
-    printf("Start tests\n");
-    UNITY_BEGIN();
-    RUN_TEST(test_variable_assignment);
-    RUN_TEST(test_multiplication);
-    sleep_ms(5000);
-    return UNITY_END();
+    TaskHandle_t main_handle;
+    xTaskCreate(tests, "Tests", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY+10, &main_handle);
+    vTaskStartScheduler();
+    return 0;
 }
